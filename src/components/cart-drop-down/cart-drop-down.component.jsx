f2b50fb5d2b60f4ed87dceefcdea194f16a677ff
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-04-20 13:07:09 -0400

    Hides checkout drop-down when user directed to Checkout

--- src/components/cart-drop-down/cart-drop-down.component.jsx
+++ src/components/cart-drop-down/cart-drop-down.component.jsx
@@ -1,9 +1,13 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import CartItem from "../cart-item/cart-item.component";
 import Button from "../button/button.comopnent";
-import { selectCartItems } from "../../store/cart/cart.selector";
+import {
+  selectCartItems,
+  selectIsCartOpen,
+} from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 import {
   CartDropDownContainer,
@@ -12,10 +16,14 @@ import {
 } from "./cart-drop-down.styles.jsx";
 
 const CartDropDown = () => {
+  const dispatch = useDispatch();
+
   const cartItems = useSelector(selectCartItems);
+  const isCartOpen = useSelector(selectIsCartOpen);
   const navigate = useNavigate();
 
   const gotToCheckoutHandler = () => {
+    dispatch(setIsCartOpen(!isCartOpen));
     navigate("/checkout");
   };
 
